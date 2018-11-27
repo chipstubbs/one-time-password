de3c402c1cd84cdf913d0125275811e7be4b3e30
Chip Stubbs <cstubbs@advisorsacademy.com>
2018-11-27 17:03:57 -0500

    6.53 near finished

--- functions/create_user.js
+++ functions/create_user.js
@@ -2,9 +2,12 @@ const admin = require('firebase-admin');
 
 module.exports = (req, res) => {
     // Verify the user provided a phone
-    
+    if (!req.body.phone) {
+        return res.status(422).send({ error: 'Bad Input' });
+    }
 
     // Format the phone number to remove dashes and parenthesis
+    const phone = String(req.body.phone).replace(/[^\d]/g);
 
     // Create a new user account using that phone number
 
