cf198840ef226f6c770a2fa94855b1608a9da499
Chip Stubbs <cstubbs@advisorsacademy.com>
2018-11-27 16:46:02 -0500

    initial

---
+++ functions/create_user.js
@@ -0,0 +1,12 @@
+const admin = require('firebase-admin');
+
+module.exports = (req, res) => {
+    // Verify the user provided a phone
+    
+
+    // Format the phone number to remove dashes and parenthesis
+
+    // Create a new user account using that phone number
+
+    // Respond to the user request, saying the account was made
+}
---
+++ functions/index.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+const serviceAccount = requre('./service_account.json');
+const functions = require('firebase-functions');
+const createUser = require('./create_user');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://one-time-password-eb048.firebaseio.com"
+});
+
+exports.createUser = functions.https.onRequest(createUser);
