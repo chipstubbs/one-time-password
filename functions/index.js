fe33d228fa197d5716fb04c4a78512d623570850
chipstubbs <chip.stubbs@icloud.com>
2018-12-04 13:31:12 -0500

    fixed spelling error

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = requre('./service_account.json');
+const serviceAccount = require('./service_account.json');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
 
